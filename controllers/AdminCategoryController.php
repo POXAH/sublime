52eb6be7c511103281bf0749341ac97a1aa21b33
Ruslan <crezi_23@mail.ru>
2019-04-30 11:20:14 +0500

    Update AdminCategoryController.php

--- controllers/AdminCategoryController.php
+++ controllers/AdminCategoryController.php
@@ -10,14 +10,9 @@ use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 
-/**
- * AdminCategoryController implements the CRUD actions for Category model.
- */
 class AdminCategoryController extends Controller
 {
-    /**
-     * {@inheritdoc}
-     */
+
     public function behaviors()
     {
         return [
@@ -30,10 +25,6 @@ class AdminCategoryController extends Controller
         ];
     }
 
-    /**
-     * Lists all Category models.
-     * @return mixed
-     */
     public function actionIndex()
     {
 
@@ -57,12 +48,6 @@ class AdminCategoryController extends Controller
         }
     }
 
-    /**
-     * Displays a single Category model.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
     public function actionView($id)
     {
         $this->layout = 'admin-layout';
@@ -72,11 +57,6 @@ class AdminCategoryController extends Controller
         ]);
     }
 
-    /**
-     * Creates a new Category model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
     public function actionCreate()
     {
         $this->layout = 'admin-layout';
@@ -92,13 +72,6 @@ class AdminCategoryController extends Controller
         ]);
     }
 
-    /**
-     * Updates an existing Category model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
     public function actionUpdate($id)
     {
         $this->layout = 'admin-layout';
@@ -114,13 +87,6 @@ class AdminCategoryController extends Controller
         ]);
     }
 
-    /**
-     * Deletes an existing Category model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
     public function actionDelete($id)
     {
         $this->findModel($id)->delete();
@@ -128,13 +94,6 @@ class AdminCategoryController extends Controller
         return $this->redirect(['index']);
     }
 
-    /**
-     * Finds the Category model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return Category the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
     protected function findModel($id)
     {
         if (($model = Category::findOne($id)) !== null) {
