58f8e308384e8774a80744ce1c557db486de219b
Ruslan <crezi_23@mail.ru>
2019-04-30 10:14:09 +0500

    Update Product.php

--- models/Product.php
+++ models/Product.php
@@ -4,6 +4,7 @@
 namespace app\models;
 
 
+use Yii;
 use yii\data\Pagination;
 use yii\db\ActiveRecord;
 
@@ -22,48 +23,56 @@ class Product extends ActiveRecord
 
     public function getLastProducts($count)
     {
-        $products = Product::find()
-            ->orderBy(['id'=>SORT_DESC])
-            ->limit($count)
-            ->asArray()
-            ->all();
+        $products = Yii::$app->cache->get('last_products');
+        if (!$products){
+            $products = Product::find()
+                ->orderBy(['id'=>SORT_DESC])
+                ->limit($count)
+                ->asArray()
+                ->all();
+            Yii::$app->cache->set('last_products', $products, 10);
+        }
+
         return $products;
     }
 
 
     public function getOneProducts($link_name)
     {
-        $product = Product::find()
-            ->where(['link_name' => $link_name])
-            ->one();
+        $product = Yii::$app->cache->get('product_'.$link_name);
+        if (!$product){
+            $product = Product::find()
+                ->where(['link_name' => $link_name])
+                ->one();
+            Yii::$app->cache->set('product_'.$link_name, $product, 60);
+        }
         return $product;
     }
-
-//    public function getCategoryProducts($category)
-//    {
-//        $products = Product::find()
-//            ->where(['id_category' => $category])
-//            ->asArray()
-//            ->all();
-//        return $products;
-//    }
 
 
     public function getCategoryProductsWithLimit($category, $count)
     {
-        $products = Product::find()
-            ->where(['id_category' => $category])
-            ->limit($count)
-            ->asArray()
-            ->all();
+        $products = Yii::$app->cache->get('products_limit_'.$category);
+        if (!$products){
+            $products = Product::find()
+                ->where(['id_category' => $category])
+                ->limit($count)
+                ->asArray()
+                ->all();
+            Yii::$app->cache->set('products_limit_'.$category, $products, 60);
+        }
         return $products;
     }
 
     public function getSearchResult($search)
     {
-        $searchResult = Product::find()
-            ->where(['like','name', $search])
-            ->orWhere(['like', 'description', $search]);
+        $searchResult = Yii::$app->cache->get('search_'.$search);
+        if (!$searchResult){
+            $searchResult = Product::find()
+                ->where(['like','name', $search])
+                ->orWhere(['like', 'description', $search]);
+            Yii::$app->cache->set('search_'.$search, $searchResult, 60);
+        }
 //            ->asArray()
 //            ->all();
         return $searchResult;
